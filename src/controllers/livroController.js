9af6c0929571e671002f60b5017c6e88953a9b5e
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-05-20 21:50:12 -0300

    20-05

---
+++ src/controllers/livroController.js
@@ -0,0 +1,12 @@
+import livro from '../models/Livros.js'
+class LivroController {
+    static async listarLivros (req, res){
+        try {
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - falha na requisição`});
+        }
+    };
+}
+export default LivroController;
