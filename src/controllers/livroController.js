6e30ccf416d0f36a78cbb1b7b6d8a6d432d72d74
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-05-22 21:46:04 -0300

    api completa

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -9,6 +9,16 @@ class LivroController {
         }
     };
 
+    static async listarLivroPorId(req, res) {
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+            res.status(200).json(livroEncontrado);
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na requisição do livro` });
+        }
+    };
+
     static async cadastrarLivro(req, res) {
         try {
             const novoLivro = await livro.create(req.body);
@@ -17,6 +27,27 @@ class LivroController {
             res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro` });
         }
     }
+
+    static async atualizarLivro(req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: "livro atualizado" });
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na atualização` });
+        }
+    };
+
+    static async excluirLivro (req, res){
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res.status(200).json({ message: "Livro Excluído!" });
+            
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha ao deletar` });
+        }
+    };
 
 }
 export default LivroController;
